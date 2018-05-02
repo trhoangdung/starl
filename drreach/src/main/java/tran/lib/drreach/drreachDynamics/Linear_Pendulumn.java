e8b81dfe3e50452895da465454e021a5b42ed16a
trhoangdung <trhoangdung@gmail.com>
2018-05-02 16:26:02 -0500

    add linear pedulum dynamics, working toward get_derivative_bounds

--- drreach/src/main/java/tran/lib/drreach/drreachDynamics/Linear_Pendulumn.java
+++ drreach/src/main/java/tran/lib/drreach/drreachDynamics/Linear_Pendulumn.java
@@ -13,8 +13,10 @@ public class Linear_Pendulumn {
     // x' = Ax + Bu, u = Kx, u between -4.95 and 4.95
     // where x = [pos_err velocity angle angular_vel]^T
 
-    static final double[][] A = {{0, 1, 0, 0}, {0, -10.95, -2.75, 0.0043}, {0, 0, 0, 1}, {0, 24.92, 28.58, -0.044}}; // matrix A
-    static final double[] B = {0, 1.94, 0, -4.44}; // B transposed
-    static final double[] K = {0.4072, 7.2373, 18.6269, 3.6725}; // K
+    static final double[][] A = {{0, 1, 0, 0}, {0, -10.95, -2.75, 0.0043}, {0, 0, 0, 1}, {0, 24.92, 28.58, -0.044}}; // matrix A: 4 x 4
+    static final double[][] B = {{0}, {1.94}, {0}, {-4.44}}; // matrix B: 4 x 1
+    static final double[] K = {0.4072, 7.2373, 18.6269, 3.6725}; // matrix K: 1 x 4
+
+
 
 }
